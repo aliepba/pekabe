c6d28ae4df5fa5d73c6bc477b1a59af1ee906702
hitsugayakun <aliefbagas04@gmail.com>
2023-02-25 19:14:03 +0700

    fixing query asosiasi pendaftaran

--- app/Actions/Asosiasi/AsosiasiList.php
+++ app/Actions/Asosiasi/AsosiasiList.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Asosiasi;
 
 use App\Models\DetailInstansi;
+use App\Enums\PermohonanStatus;
 use App\Models\MtAsosiasiProfesi;
 use App\Models\MtAsosiasiBadanUsaha;
 use Lorisleiva\Actions\Concerns\AsAction;
@@ -15,10 +16,10 @@ class AsosiasiList
     {
         return [
             'profesi' => MtAsosiasiProfesi::query()
-                    ->whereNotIn('ID_Asosiasi_Profesi',DetailInstansi::where('jenis', 2)->get())
+                    ->whereNotIn('ID_Asosiasi_Profesi',DetailInstansi::where('jenis', 2)->where('status_permohonan', PermohonanStatus::APPROVE)->get('penyelenggara'))
                     ->get(),
             'badanUsaha' => MtAsosiasiBadanUsaha::query()
-                    ->whereNotIn('ID_Asosiasi_BU',DetailInstansi::where('jenis', 3)->get())
+                    ->whereNotIn('ID_Asosiasi_BU',DetailInstansi::where('jenis', 3)->where('status_permohonan', PermohonanStatus::APPROVE)->get('penyelenggara'))
                     ->get(),
             ];
     }
