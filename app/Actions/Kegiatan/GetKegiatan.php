13f65798d58c93dc1de20e0634806828ec8d9910
aliepba <aliefbagas04@gmail.com>
2023-06-19 10:16:28 +0700

    update query kegiatan setujui

--- app/Actions/Kegiatan/GetKegiatan.php
+++ app/Actions/Kegiatan/GetKegiatan.php
@@ -17,6 +17,7 @@ class GetKegiatan
             'setuju' => Kegiatan::where('status_permohonan_kegiatan', '!=' ,PermohonanStatus::OPEN)
                                 ->where('status_permohonan_kegiatan', '!=', PermohonanStatus::SUBMIT)
                                 ->where('status_permohonan_kegiatan', '!=', PermohonanStatus::TOLAK)
+                                ->where('status_permohonan_kegiatan', '=', PermohonanStatus::PERBAIKAN_PELAPORAN)
                                 ->get(),
             'tolak' => Kegiatan::where('status_permohonan_kegiatan', PermohonanStatus::TOLAK)->get()
         ];
