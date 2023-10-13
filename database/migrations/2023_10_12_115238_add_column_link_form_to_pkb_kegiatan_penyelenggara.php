2e2210e1d635a8bc986735285de6a50545d4638f
aliepba <aliefbagas04@gmail.com>
2023-10-13 10:12:33 +0700

    setting redis and alter table

---
+++ database/migrations/2023_10_12_115238_add_column_link_form_to_pkb_kegiatan_penyelenggara.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnLinkFormToPkbKegiatanPenyelenggara extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pkb_kegiatan_penyelenggara', function (Blueprint $table) {
+            $table->string('link_form')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pkb_kegiatan_penyelenggara', function (Blueprint $table) {
+            $table->string('link_form')->nullable();
+        });
+    }
+}
